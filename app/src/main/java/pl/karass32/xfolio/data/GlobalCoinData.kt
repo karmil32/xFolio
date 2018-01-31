c253d0206e3496a75dc54797dd13c02c0c2b472f
karas <karass32@gmail.com>
2018-01-31 18:30:16 +0100

    add SerializedName annotations

--- app/src/main/java/pl/karass32/xfolio/data/GlobalCoinData.kt
+++ app/src/main/java/pl/karass32/xfolio/data/GlobalCoinData.kt
@@ -1,14 +1,15 @@
 package pl.karass32.xfolio.data
 
+import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
 
 /**
  * Created by karas on 24.01.2018.
  */
-data class GlobalCoinData(val totalMarketCap: BigDecimal,
-                          val total24hVolume: BigDecimal,
-                          val bitcoinDominance: BigDecimal,
-                          val activeCurrencies: Int,
-                          val activeAssets: Int,
-                          val activeMarkets: Int,
-                          val lastUpdated: Long)
+data class GlobalCoinData(@SerializedName("total_market_cap_usd") val totalMarketCap: BigDecimal,
+                          @SerializedName("total_24h_volume_usd") val total24hVolume: BigDecimal,
+                          @SerializedName("bitcoin_percentage_of_market_cap") val bitcoinDominance: BigDecimal,
+                          @SerializedName("active_currencies") val activeCurrencies: Int,
+                          @SerializedName("active_assets") val activeAssets: Int,
+                          @SerializedName("active_markets") val activeMarkets: Int,
+                          @SerializedName("last_updated") val lastUpdated: Long)
