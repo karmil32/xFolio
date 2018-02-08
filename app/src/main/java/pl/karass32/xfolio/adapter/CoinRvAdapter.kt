f8b608d5dc463ffdfef21f3d04489183a9f5d283
karas <karass32@gmail.com>
2018-02-08 11:29:03 +0100

    refactoring

--- app/src/main/java/pl/karass32/xfolio/adapter/CoinRvAdapter.kt
+++ app/src/main/java/pl/karass32/xfolio/adapter/CoinRvAdapter.kt
@@ -11,8 +11,8 @@ import pl.karass32.xfolio.data.CoinData
 import pl.karass32.xfolio.extension.getColor
 import pl.karass32.xfolio.repository.api.CoinMarketCapService
 import pl.karass32.xfolio.repository.api.GlideApp
+import pl.karass32.xfolio.util.NumberUtils
 import java.math.BigDecimal
-import java.text.DecimalFormat
 
 
 /**
@@ -20,25 +20,15 @@ import java.text.DecimalFormat
  */
 class CoinRvAdapter(private val coins: ArrayList<CoinData>) : RecyclerView.Adapter<CoinRvAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.coin_rv_layout, parent, false))
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.coin_rv_layout, parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(coins[position])
 
-    override fun getItemCount(): Int {
-        return coins.size
-    }
+    override fun getItemCount() = coins.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         @SuppressLint("SetTextI18n")
         fun bind(coinData: CoinData) = with(itemView) {
-
-            val pricePattern = if (coinData.price > BigDecimal(1)) "###,###.00" else "#.####"
-            val priceFormat = DecimalFormat(pricePattern)
-
-            val percentagePattern = "#0.00"
-            val percentageFormat = DecimalFormat(percentagePattern)
 
             GlideApp.with(this)
                     .load("${CoinMarketCapService.API_IMAGES_URL}${coinData.id}.png")
@@ -48,8 +38,8 @@ class CoinRvAdapter(private val coins: ArrayList<CoinData>) : RecyclerView.Adapt
             coinRvRankNumber.text = coinData.rank.toString()
             coinRvName.text = coinData.name
             coinRvSymbol.text = "(${coinData.symbol})"
-            coinRvPrice.text = "$${priceFormat.format(coinData.price)}"
-            coinRvChange.text = "${percentageFormat.format(coinData.change24h)}%"
+            coinRvPrice.text = "$${NumberUtils.getPriceFormat(coinData.price).format(coinData.price)}"
+            coinRvChange.text = "${NumberUtils.percentageFormat.format(coinData.change24h)}%"
 
             coinRvChange.setTextColor(if (coinData.change24h >= BigDecimal(0)) getColor(R.color.positiveColor) else getColor(R.color.negativeColor))
         }
