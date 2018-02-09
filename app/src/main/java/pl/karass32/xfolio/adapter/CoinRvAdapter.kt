ff478f72819ba69a54fd8fb5dddf8f526b6a0f02
karas <karass32@gmail.com>
2018-02-09 11:15:15 +0100

    implements filterable

--- app/src/main/java/pl/karass32/xfolio/adapter/CoinRvAdapter.kt
+++ app/src/main/java/pl/karass32/xfolio/adapter/CoinRvAdapter.kt
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import kotlinx.android.synthetic.main.coin_rv_layout.view.*
 import pl.karass32.xfolio.R
 import pl.karass32.xfolio.data.CoinData
@@ -18,13 +20,17 @@ import java.math.BigDecimal
 /**
  * Created by karas on 15.01.2018.
  */
-class CoinRvAdapter(private val coins: ArrayList<CoinData>) : RecyclerView.Adapter<CoinRvAdapter.ViewHolder>() {
+class CoinRvAdapter(private var coinList: ArrayList<CoinData>) : RecyclerView.Adapter<CoinRvAdapter.ViewHolder>(), Filterable {
+
+    private val mCoinListCopy by lazy {
+        ArrayList<CoinData>(coinList)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.coin_rv_layout, parent, false))
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(coins[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(coinList[position])
 
-    override fun getItemCount() = coins.size
+    override fun getItemCount() = coinList.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         @SuppressLint("SetTextI18n")
@@ -42,6 +48,35 @@ class CoinRvAdapter(private val coins: ArrayList<CoinData>) : RecyclerView.Adapt
             coinRvChange.text = "${NumberUtils.percentageFormat.format(coinData.change24h)}%"
 
             coinRvChange.setTextColor(if (coinData.change24h >= BigDecimal(0)) getColor(R.color.positiveColor) else getColor(R.color.negativeColor))
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val charString = p0.toString()
+                if (charString.isEmpty()) {
+                    coinList = mCoinListCopy
+                } else {
+                    val filteredList = ArrayList<CoinData>()
+                    for (coinData in mCoinListCopy) {
+                        if (coinData.name.toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(coinData)
+                        }
+                    }
+
+                    coinList = filteredList
+                }
+
+                val filterResults = FilterResults()
+                filterResults.values = coinList
+                return filterResults
+            }
+
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+                coinList = p1?.values as ArrayList<CoinData>
+                notifyDataSetChanged()
+            }
         }
     }
 }
