22fbc6f175b6c58c7b18edae2dc09eed9f95e745
karas <karass32@gmail.com>
2018-02-09 11:22:55 +0100

    add filter option

--- app/src/main/java/pl/karass32/xfolio/ui/coinlist/CoinListFragment.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/coinlist/CoinListFragment.kt
@@ -7,9 +7,8 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.SearchView
+import android.view.*
 import android.widget.Toast
 import kotlinx.android.synthetic.main.coin_global_data_layout.view.*
 import kotlinx.android.synthetic.main.coin_list_fragment.view.*
@@ -29,6 +28,7 @@ class CoinListFragment : Fragment() {
 
     private lateinit var mainActivity: MainActivity
     private lateinit var mView: View
+    private lateinit var mCoinRvAdapter: CoinRvAdapter
 
     private val mViewModel: CoinListViewModel by lazy {
         ViewModelProviders.of(mainActivity).get(CoinListViewModel::class.java)
@@ -36,6 +36,8 @@ class CoinListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.coin_list_fragment, container, false)
+
+        setHasOptionsMenu(true)
 
         mainActivity = activity as MainActivity
         mainActivity.setSupportActionBar(mView.toolbar)
@@ -47,6 +49,28 @@ class CoinListFragment : Fragment() {
         initViewModel()
 
         return mView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.coin_list_menu, menu)
+
+        val searchItem = menu.findItem(R.id.search_item)
+        val searchView = searchItem.actionView as SearchView
+        searchView.queryHint = getString(R.string.toolbar_menu_search_hint)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                mCoinRvAdapter.filter.filter(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                mCoinRvAdapter.filter.filter(newText)
+                return true
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     private fun initSwipeRefreshLayout() {
@@ -88,9 +112,10 @@ class CoinListFragment : Fragment() {
     }
 
     private fun showList(list: ArrayList<CoinData>) {
+        mCoinRvAdapter = CoinRvAdapter(list)
         mView.coinListRv?.setHasFixedSize(true)
         mView.coinListRv?.layoutManager = LinearLayoutManager(mainActivity)
-        mView.coinListRv?.adapter = CoinRvAdapter(list)
+        mView.coinListRv?.adapter = mCoinRvAdapter
     }
 
     private fun onCoinListError(error: CoinListErrorEvent) {
