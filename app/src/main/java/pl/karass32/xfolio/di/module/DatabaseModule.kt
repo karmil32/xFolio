755a2845b0f2ddddf8a36901ee117a3fc7d890a3
karas <karass32@gmail.com>
2018-02-21 00:31:26 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/di/module/DatabaseModule.kt
@@ -0,0 +1,23 @@
+package pl.karass32.xfolio.di.module
+
+import android.app.Application
+import android.arch.persistence.room.Room
+import dagger.Module
+import dagger.Provides
+import pl.karass32.xfolio.repository.db.AppDatabase
+import javax.inject.Singleton
+
+/**
+ * Created by karas on 21.02.2018.
+ */
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application) : AppDatabase = Room.databaseBuilder(application, AppDatabase::class.java, "database")
+            .allowMainThreadQueries()
+            .build()
+    @Provides
+    @Singleton
+    fun provideCoinListDao(database: AppDatabase) = database.coinDataDao()
+}
