23ebe416eec974ebbe760b25ceb9099fb11456b3
karas <karass32@gmail.com>
2018-02-21 00:07:22 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/repository/db/CoinDataDao.kt
@@ -0,0 +1,20 @@
+package pl.karass32.xfolio.repository.db
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import android.arch.persistence.room.Query
+import pl.karass32.xfolio.data.CoinData
+
+/**
+ * Created by karas on 20.02.2018.
+ */
+@Dao
+interface CoinDataDao {
+
+    @Query ("SELECT * FROM coin_data_list")
+    fun getAllCoinData() : List<CoinData>
+
+    @Insert (onConflict = REPLACE)
+    fun insertCoinData(list: List<CoinData>)
+}
