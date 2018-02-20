81499afa583f1a5e12103d4ec0b62a831b1aa5fc
karas <karass32@gmail.com>
2018-02-21 00:31:40 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/di/component/AppComponent.kt
@@ -0,0 +1,18 @@
+package pl.karass32.xfolio.di.component
+
+import android.app.Application
+import dagger.Component
+import pl.karass32.xfolio.di.module.AppModule
+import pl.karass32.xfolio.di.module.DatabaseModule
+import pl.karass32.xfolio.ui.coinlist.CoinListViewModel
+import javax.inject.Singleton
+
+/**
+ * Created by karas on 20.02.2018.
+ */
+@Singleton
+@Component (modules = [AppModule::class, DatabaseModule::class])
+interface AppComponent {
+    fun inject(app: Application)
+    fun inject(coinListViewModel: CoinListViewModel)
+}
