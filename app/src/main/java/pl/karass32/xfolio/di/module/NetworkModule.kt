39c696e01d93abe6a33b356583752e1eb47cf552
karas <karass32@gmail.com>
2018-02-21 00:52:58 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/di/module/NetworkModule.kt
@@ -0,0 +1,16 @@
+package pl.karass32.xfolio.di.module
+
+import dagger.Module
+import dagger.Provides
+import pl.karass32.xfolio.repository.api.CoinMarketCapService
+import javax.inject.Singleton
+
+/**
+ * Created by karas on 21.02.2018.
+ */
+@Module
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideCoinMarketCapAPI() = CoinMarketCapService.create()
+}
