1e8236bce6941e45f5e6b7d2ec3e3dd9e3691c71
karas <karass32@gmail.com>
2018-02-21 02:06:28 +0100

    added cache support

--- app/src/main/java/pl/karass32/xfolio/ui/coinlist/CoinListViewModel.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/coinlist/CoinListViewModel.kt
@@ -6,30 +6,37 @@ import android.arch.lifecycle.ViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import pl.karass32.xfolio.MyApplication
 import pl.karass32.xfolio.data.CoinData
 import pl.karass32.xfolio.data.GlobalCoinData
 import pl.karass32.xfolio.error.CoinListErrorEvent
 import pl.karass32.xfolio.extension.SingleLiveEvent
 import pl.karass32.xfolio.repository.api.CoinMarketCapService
+import pl.karass32.xfolio.repository.db.CoinDataDao
+import pl.karass32.xfolio.repository.db.GlobalCoinDataDao
 import pl.karass32.xfolio.util.CoinOrder
+import javax.inject.Inject
 import kotlin.collections.ArrayList
 
 /**
  * Created by karas on 01.02.2018.
  */
 class CoinListViewModel : ViewModel() {
-
-    private val coinMarketCapService by lazy {
-        CoinMarketCapService.create()
-    }
-
     private var globalDataDisposable: Disposable? = null
     private var globalCoinData: MutableLiveData<GlobalCoinData>? = null
 
     private var coinListDisposable: Disposable? = null
-    private var coinList: MutableLiveData<ArrayList<CoinData>>? = null
+    private var coinList: MutableLiveData<List<CoinData>>? = null
 
     var coinListError = SingleLiveEvent<CoinListErrorEvent>()
+
+    @Inject lateinit var coinMarketCapService: CoinMarketCapService
+    @Inject lateinit var coinDataDao: CoinDataDao
+    @Inject lateinit var globalCoinDataDao: GlobalCoinDataDao
+
+    init {
+        MyApplication.component.inject(this)
+    }
 
     fun getGlobalCoinData() : LiveData<GlobalCoinData>? {
         if (globalCoinData == null) {
@@ -46,14 +53,15 @@ class CoinListViewModel : ViewModel() {
                 .subscribe(
                         {result ->
                             globalCoinData?.value = result
+                            globalCoinDataDao.insertGlobalCoinData(result)
                         },
                         {_ ->
-
+                            globalCoinData?.value = globalCoinDataDao.getGlobalCoinData()
                         }
                 )
     }
 
-    fun getCoinList() : LiveData<ArrayList<CoinData>>? {
+    fun getCoinList() : LiveData<List<CoinData>>? {
         if (coinList == null) {
             coinList = MutableLiveData()
             loadCoinList()
@@ -68,9 +76,11 @@ class CoinListViewModel : ViewModel() {
                 .subscribe(
                         { result ->
                             coinList?.value = result
+                            coinDataDao.insertCoinData(result)
                         },
                         { _ ->
                             coinListError.value = CoinListErrorEvent.NO_SERVER_CONNECTION
+                            coinList?.value = coinDataDao.getAllCoinData()
                         }
                 )
     }
