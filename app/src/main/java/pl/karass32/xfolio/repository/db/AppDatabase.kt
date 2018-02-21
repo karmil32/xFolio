27eb7c611c2c74ef902032bdd792618f5c58e998
karas <karass32@gmail.com>
2018-02-21 02:05:37 +0100

    added GlobalCoinData

--- app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
+++ app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
@@ -2,13 +2,15 @@ package pl.karass32.xfolio.repository.db
 
 import android.arch.persistence.room.*
 import pl.karass32.xfolio.data.CoinData
+import pl.karass32.xfolio.data.GlobalCoinData
 
 /**
  * Created by karas on 20.02.2018.
  */
-@Database(entities = [CoinData::class], version = 1)
+@Database(entities = [CoinData::class, GlobalCoinData::class], version = 1)
 @TypeConverters(DbTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun coinDataDao(): CoinDataDao
+    abstract fun globalCoinDataDao(): GlobalCoinDataDao
 }
