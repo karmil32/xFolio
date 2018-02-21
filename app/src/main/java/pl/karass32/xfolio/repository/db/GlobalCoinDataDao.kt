7bfc42ec619185085183d7de040990698f76baa2
karas <karass32@gmail.com>
2018-02-21 02:05:17 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/repository/db/GlobalCoinDataDao.kt
@@ -0,0 +1,21 @@
+package pl.karass32.xfolio.repository.db
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import android.arch.persistence.room.Query
+import pl.karass32.xfolio.data.GlobalCoinData
+
+/**
+ * Created by karas on 21.02.2018.
+ */
+@Dao
+interface GlobalCoinDataDao {
+
+    @Query("SELECT * FROM global_coin_data WHERE id = 0")
+    fun getGlobalCoinData(): GlobalCoinData
+
+    @Insert(onConflict = REPLACE)
+    fun insertGlobalCoinData(globalCoinData: GlobalCoinData)
+
+}
