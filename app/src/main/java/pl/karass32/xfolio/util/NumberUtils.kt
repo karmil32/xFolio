686058207c477500cad222f1b9ea1aed0aab8c43
karas <karass32@gmail.com>
2018-02-22 11:17:39 +0100

    change price format

--- app/src/main/java/pl/karass32/xfolio/util/NumberUtils.kt
+++ app/src/main/java/pl/karass32/xfolio/util/NumberUtils.kt
@@ -12,7 +12,7 @@ class NumberUtils {
     companion object {
         val percentageFormat = DecimalFormat("#0.00")
         val bigValueFormat = DecimalFormat("###,###.##")
-        fun getPriceFormat(price: BigDecimal) = DecimalFormat(if (price > BigDecimal(1)) "###,###.00" else "#.####")
+        fun getPriceFormat(price: BigDecimal) = DecimalFormat(if (price > BigDecimal(1)) "###,###.00" else "#.######")
 
         val dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm")
     }
