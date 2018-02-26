8b0d6211682274b1ddb177fea94ea34419be555a
karas <karass32@gmail.com>
2018-02-26 18:43:25 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/util/enum/ChangeOption.kt
@@ -0,0 +1,10 @@
+package pl.karass32.xfolio.util.enum
+
+/**
+ * Created by karas on 26.02.2018.
+ */
+enum class ChangeOption(val value: Int) {
+    CHANGE_1H(0),
+    CHANGE_24H(1),
+    CHANGE_7D(2);
+}
