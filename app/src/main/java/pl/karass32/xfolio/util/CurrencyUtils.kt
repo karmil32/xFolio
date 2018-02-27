369ad724dd8c1d88f2a6e2caaf51eac46f39782b
karas <karass32@gmail.com>
2018-02-27 14:28:15 +0100

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/util/CurrencyUtils.kt
@@ -0,0 +1,32 @@
+package pl.karass32.xfolio.util
+
+import java.util.*
+
+/**
+ * Created by karas on 27.02.2018.
+ */
+class CurrencyUtils {
+    companion object {
+        fun getCurrencySymbol(currencyCode: String) : String {
+            val currency = Currency.getInstance(currencyCode)
+            return currency.getSymbol(currencyLocaleMap[currency])
+        }
+
+        private val currencyLocaleMap: SortedMap<Currency, Locale> by lazy {
+            getCurrencyLocaleMap()
+        }
+
+        private fun getCurrencyLocaleMap(): TreeMap<Currency, Locale> {
+            val map = TreeMap<Currency, Locale>(Comparator<Currency> { p0, p1 ->
+                p0.currencyCode.compareTo(p1.currencyCode) })
+
+            for (locale in Locale.getAvailableLocales()) {
+                try {
+                    val currency = Currency.getInstance(locale)
+                    currencyLocaleMap[currency] = locale
+                } catch (e: Exception) {}
+            }
+            return map
+        }
+    }
+}
