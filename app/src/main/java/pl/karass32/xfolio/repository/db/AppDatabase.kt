479c810e3fb6e3c94ae4c223dc9fb55bd27cf46c
karas <karass32@gmail.com>
2018-03-04 22:52:54 +0100

    add fiarRatesDao

--- app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
+++ app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
@@ -2,15 +2,17 @@ package pl.karass32.xfolio.repository.db
 
 import android.arch.persistence.room.*
 import pl.karass32.xfolio.data.CoinData
+import pl.karass32.xfolio.data.FiatRate
 import pl.karass32.xfolio.data.GlobalCoinData
 
 /**
  * Created by karas on 20.02.2018.
  */
-@Database(entities = [CoinData::class, GlobalCoinData::class], version = 1)
+@Database(entities = [CoinData::class, GlobalCoinData::class, FiatRate::class], version = 2)
 @TypeConverters(DbTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun coinDataDao(): CoinDataDao
     abstract fun globalCoinDataDao(): GlobalCoinDataDao
+    abstract fun fiatRatesDao(): FiatRatesDao
 }
