d0faeb3cc8fd0df1d3c1f4719482fde4d4c24fa6
karas <karass32@gmail.com>
2018-03-06 12:44:59 +0100

    new image url

--- app/src/main/java/pl/karass32/xfolio/repository/api/CoinMarketCapService.kt
+++ app/src/main/java/pl/karass32/xfolio/repository/api/CoinMarketCapService.kt
@@ -21,7 +21,7 @@ interface CoinMarketCapService {
 
     companion object {
         const val API_BASE_URL = "https://api.coinmarketcap.com/v1/"
-        const val API_IMAGES_URL = "https://files.coinmarketcap.com/static/img/coins/32x32/"
+        const val API_IMAGES_URL = "https://images.coinviewer.io/currencies/32x32/"
 
         fun create(): CoinMarketCapService {
             val retrofit = Retrofit.Builder()
