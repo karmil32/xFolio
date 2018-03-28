74118b72117f80f8455ab162fdf7b6b7f56cbb17
karas <karass32@gmail.com>
2018-03-28 17:41:01 +0200

    replace Int with String values

--- app/src/main/java/pl/karass32/xfolio/util/CoinOrder.kt
+++ app/src/main/java/pl/karass32/xfolio/util/CoinOrder.kt
@@ -3,13 +3,13 @@ package pl.karass32.xfolio.util
 /**
  * Created by karas on 13.02.2018.
  */
-enum class CoinOrder(val value : Int) {
-    BY_MARKET_CAP_DSC(10),
-    BY_MARKET_CAP_ASC(11),
-    BY_PRICE_DSC(20),
-    BY_PRICE_ASC(21),
-    BY_CHANGE_1H_DSC(30),
-    BY_CHANGE_1H_ASC(31),
-    BY_CHANGE_24H_DSC(40),
-    BY_CHANGE_24H_ASC(41)
+enum class CoinOrder(val value : String) {
+    BY_MARKET_CAP_DSC("coin_list_order_by_market_cap_DSC"),
+    BY_MARKET_CAP_ASC("coin_list_order_by_market_cap_ASC"),
+    BY_PRICE_DSC("coin_list_order_by_price_DSC"),
+    BY_PRICE_ASC("coin_list_order_by_price_ASC"),
+    BY_CHANGE_1H_DSC("coin_list_order_by_change_1h_DSC"),
+    BY_CHANGE_1H_ASC("coin_list_order_by_change_1h_ASC"),
+    BY_CHANGE_24H_DSC("coin_list_order_by_change_24h_DSC"),
+    BY_CHANGE_24H_ASC("coin_list_order_by_change_24h_ASC")
 }
