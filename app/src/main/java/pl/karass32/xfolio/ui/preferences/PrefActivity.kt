4522f14d81e2e212dc5225fa9f1d2e692303cb12
karas <karass32@gmail.com>
2018-04-02 16:10:49 +0200

    add actionBar

--- app/src/main/java/pl/karass32/xfolio/ui/preferences/PrefActivity.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/preferences/PrefActivity.kt
@@ -1,9 +1,29 @@
 package pl.karass32.xfolio.ui.preferences
 
+import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.support.design.widget.AppBarLayout
+import android.support.v7.widget.Toolbar
 import pl.karass32.xfolio.R
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+
+
 
 class PrefActivity : PreferenceActivity() {
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        val root = findViewById<View>(android.R.id.list).parent.parent.parent as LinearLayout
+        val bar = LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false) as AppBarLayout
+        root.addView(bar, 0) // insert at top
+
+        val toolbar = bar.getChildAt(0) as Toolbar
+        toolbar.title = getString(R.string.action_settings)
+        toolbar.setNavigationOnClickListener { finish() }
+    }
 
     override fun onBuildHeaders(target: MutableList<Header>?) {
         loadHeadersFromResource(R.xml.headers_preference, target)
