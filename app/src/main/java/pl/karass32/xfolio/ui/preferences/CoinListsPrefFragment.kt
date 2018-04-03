7e0b0bfdd1d10c5db12744dc6041cfeec5ef5eb0
karas <karass32@gmail.com>
2018-04-03 11:32:06 +0200

    change name and xml res

--- app/src/main/java/pl/karass32/xfolio/ui/preferences/CoinListsPrefFragment.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/preferences/CoinListsPrefFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.preference.PreferenceFragment
 import pl.karass32.xfolio.R
 
-class CoinListPrefFragment : PreferenceFragment() {
+class CoinListsPrefFragment : PreferenceFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.coin_list_prefs)
+        addPreferencesFromResource(R.xml.coin_lists_prefs)
     }
 }
