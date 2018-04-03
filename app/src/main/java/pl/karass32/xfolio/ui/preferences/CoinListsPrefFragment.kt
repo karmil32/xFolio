c2691b768a8c46038364cb5b763f2f4e21581e47
karas <karass32@gmail.com>
2018-04-03 12:54:33 +0200

    add listener

--- app/src/main/java/pl/karass32/xfolio/ui/preferences/CoinListsPrefFragment.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/preferences/CoinListsPrefFragment.kt
@@ -1,13 +1,33 @@
 package pl.karass32.xfolio.ui.preferences
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.ListPreference
 import android.preference.PreferenceFragment
 import pl.karass32.xfolio.R
 
-class CoinListsPrefFragment : PreferenceFragment() {
+class CoinListsPrefFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.coin_lists_prefs)
+
+        val coinListOrder = findPreference("coin_list_order") as ListPreference
+        coinListOrder.summary = coinListOrder.entry
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val preference = findPreference(key)
+        if (preference is ListPreference) { preference.summary = preference.entry }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
     }
 }
