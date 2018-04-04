04ea02ac4d2aaeeb299abaddaf17c49a49f0818c
karas <karass32@gmail.com>
2018-04-04 19:21:52 +0200

    change value type to String, odd "of" method

--- app/src/main/java/pl/karass32/xfolio/util/enum/ChangeOption.kt
+++ app/src/main/java/pl/karass32/xfolio/util/enum/ChangeOption.kt
@@ -3,8 +3,12 @@ package pl.karass32.xfolio.util.enum
 /**
  * Created by karas on 26.02.2018.
  */
-enum class ChangeOption(val value: Int) {
-    CHANGE_1H(0),
-    CHANGE_24H(1),
-    CHANGE_7D(2);
+enum class ChangeOption(val value: String) {
+    CHANGE_1H("coin_list_change_1h"),
+    CHANGE_24H("coin_list_change_24h"),
+    CHANGE_7D("coin_list_change_7d");
+
+    companion object {
+        fun of(value: String): ChangeOption = ChangeOption.values().first { it.value == value }
+    }
 }
