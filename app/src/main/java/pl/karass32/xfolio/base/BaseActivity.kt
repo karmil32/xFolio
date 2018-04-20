01fd686d44a7672a561d4c8b32d80198c4ae2e5b
karas <karass32@gmail.com>
2018-04-20 19:24:11 +0200

    language support

--- app/src/main/java/pl/karass32/xfolio/base/BaseActivity.kt
+++ app/src/main/java/pl/karass32/xfolio/base/BaseActivity.kt
@@ -1,8 +1,13 @@
 package pl.karass32.xfolio.base
 
 import android.app.Application
+import android.content.Context
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import pl.karass32.xfolio.repository.pref.SharedPreferencesRepository
+import pl.karass32.xfolio.util.ContextWrapper
+import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -11,4 +16,14 @@ abstract class BaseActivity : AppCompatActivity() {
 
     @Inject
     lateinit var preferences: SharedPreferencesRepository
+
+    override fun attachBaseContext(base: Context) {
+        val systemLang = Locale.getDefault().country
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(base)
+        val langString = sharedPreferences.getString("general_app_language", "auto")
+        val locale = Locale(if (langString == "auto") systemLang else langString)
+
+        val context = ContextWrapper.wrap(base, locale)
+        super.attachBaseContext(context)
+    }
 }
