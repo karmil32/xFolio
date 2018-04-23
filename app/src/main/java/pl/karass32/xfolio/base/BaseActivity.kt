01f4f96b0dc56a952e70a5599bf02606c0337d6c
karas <karass32@gmail.com>
2018-04-23 19:42:04 +0200

    add DayNight support

--- app/src/main/java/pl/karass32/xfolio/base/BaseActivity.kt
+++ app/src/main/java/pl/karass32/xfolio/base/BaseActivity.kt
@@ -2,11 +2,13 @@ package pl.karass32.xfolio.base
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
+import pl.karass32.xfolio.MyApplication
 import pl.karass32.xfolio.repository.pref.SharedPreferencesRepository
 import pl.karass32.xfolio.util.ContextWrapper
-import timber.log.Timber
 import java.util.*
 import javax.inject.Inject
 
@@ -16,6 +18,12 @@ abstract class BaseActivity : AppCompatActivity() {
 
     @Inject
     lateinit var preferences: SharedPreferencesRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MyApplication.component.inject(this)
+        AppCompatDelegate.setDefaultNightMode(if (preferences.isNightModeEnabled()) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
+    }
 
     override fun attachBaseContext(base: Context) {
         val systemLang = Locale.getDefault().language
