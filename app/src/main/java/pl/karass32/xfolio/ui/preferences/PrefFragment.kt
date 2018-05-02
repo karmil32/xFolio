68ddab51511508ed885767905ff20d1705a0543f
karas <karass32@gmail.com>
2018-05-02 13:36:55 +0200

    add currency picker

--- app/src/main/java/pl/karass32/xfolio/ui/preferences/PrefFragment.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/preferences/PrefFragment.kt
@@ -2,32 +2,44 @@ package pl.karass32.xfolio.ui.preferences
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.ListPreference
+import android.preference.PreferenceFragment
 import android.support.design.widget.AppBarLayout
-import android.support.v7.preference.ListPreference
-import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import pl.karass32.xfolio.MainActivity
+import com.mynameismidori.currencypicker.CurrencyPicker
+import com.mynameismidori.currencypicker.CurrencyPreference
+import com.mynameismidori.currencypicker.ExtendedCurrency
 import pl.karass32.xfolio.MyApplication
 import pl.karass32.xfolio.R
 import pl.karass32.xfolio.repository.pref.SharedPreferencesRepositoryImpl
 
-class PrefFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+class PrefFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private val mainActivity: MainActivity by lazy { activity as MainActivity }
+    private val settingsActivity: SettingsActivity by lazy { activity as SettingsActivity }
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preference, rootKey)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.preference)
 
         val appLanguage = findPreference("general_app_language") as ListPreference
         appLanguage.summary = appLanguage.entry
         val autoOpen = findPreference("general_auto_open") as ListPreference
         autoOpen.summary = autoOpen.entry
-        val defaultCurrency = findPreference("general_default_currency") as ListPreference
-        val nightMode = findPreference("general_night_mode")
+
+
+        val supportedCurrencies = listOf("USD", "EUR", "PLN")
+        val currencies: ArrayList<ExtendedCurrency> = ArrayList()
+        currencies.apply { supportedCurrencies.forEach { currencies.add(ExtendedCurrency.getCurrencyByISO(it)) } }
+        val defaultCurrency = findPreference("general_default_currency") as CurrencyPreference
+        defaultCurrency.setCurrenciesList(currencies)
+        val currencyPicker = CurrencyPicker.newInstance("Select Currency")
+        currencyPicker.setCurrenciesList(currencies)
+        currencyPicker.setListener { name, code, symbol, p3 -> defaultCurrency.value = code }
+
         val coinListOrder = findPreference("coin_list_order") as ListPreference
         coinListOrder.summary = coinListOrder.entry
         val coinListChange = findPreference("coin_list_change") as ListPreference
@@ -42,8 +54,7 @@ class PrefFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPrefe
 
         val toolbar = bar.getChildAt(0) as Toolbar
         toolbar.title = getString(R.string.nav_settings)
-        toolbar.setNavigationOnClickListener { mainActivity.onBackPressed() }
-        mainActivity.lockDrawerLayout(true)
+        toolbar.setNavigationOnClickListener { settingsActivity.onBackPressed() }
 
         return view
     }
@@ -64,10 +75,5 @@ class PrefFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPrefe
     override fun onPause() {
         preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
         super.onPause()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        mainActivity.lockDrawerLayout(false)
     }
 }
