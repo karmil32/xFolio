5a77655d768f17d9ea9483d8314c00159afed8ce
karas <karass32@gmail.com>
2018-06-06 14:04:20 +0200

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/repository/db/FavoritesDao.kt
@@ -0,0 +1,23 @@
+package pl.karass32.xfolio.repository.db
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.IGNORE
+import android.arch.persistence.room.Query
+
+@Dao
+interface FavoritesDao {
+
+    @Query("SELECT * FROM favorites_list")
+    fun getAll(): List<FavoriteEntity>
+
+    @Insert(onConflict = IGNORE)
+    fun insert(name: FavoriteEntity)
+
+    @Query("DELETE FROM favorites_list WHERE coin_name = :name")
+    fun delete(name: String)
+
+    @Query("SELECT COUNT(*) FROM favorites_list WHERE coin_name = :name LIMIT 1")
+    fun isAdded(name: String): Boolean
+}
