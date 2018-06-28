52ce7182f1f85c213039acdd7dcc6e1d0bce735f
karas <karass32@gmail.com>
2018-06-28 19:39:31 +0200

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/ui/coindetails/CoinDetailsActivity.kt
@@ -0,0 +1,13 @@
+package pl.karass32.xfolio.ui.coindetails
+
+import android.os.Bundle
+import pl.karass32.xfolio.R
+import pl.karass32.xfolio.base.BaseActivity
+
+class CoinDetailsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.coin_details_activity)
+    }
+}
