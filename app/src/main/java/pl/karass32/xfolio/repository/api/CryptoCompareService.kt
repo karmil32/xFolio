4baf56a94a85aa4615b0307fb04a5b4df8803e1d
karas <karass32@gmail.com>
2018-07-13 11:45:33 +0200

    initial commit

---
+++ app/src/main/java/pl/karass32/xfolio/repository/api/CryptoCompareService.kt
@@ -0,0 +1,28 @@
+package pl.karass32.xfolio.repository.api
+
+import io.reactivex.Observable
+import pl.karass32.xfolio.data.HistDataResponse
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CryptoCompareService {
+
+    @GET("histoday?tsym=USD&allData=true&aggregate=1&e=CCCAGG")
+    fun getAllHistoricalData(@Query("fsym") id: String?) : Observable<HistDataResponse>
+
+    companion object {
+        const val API_BASE_URL = "https://min-api.cryptocompare.com/data/"
+
+        fun create(): CryptoCompareService {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(API_BASE_URL)
+                    .build()
+            return retrofit.create(CryptoCompareService::class.java)
+        }
+    }
+}
