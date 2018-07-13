fbce8da0ebc1d123a3c84742970173e612433ab8
karas <karass32@gmail.com>
2018-07-13 11:45:09 +0200

    add histData

--- app/src/main/java/pl/karass32/xfolio/ui/coindetails/CoinDetailsViewModel.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/coindetails/CoinDetailsViewModel.kt
@@ -4,24 +4,30 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Transformations
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import pl.karass32.xfolio.base.BaseViewModel
 import pl.karass32.xfolio.data.CoinData
 import pl.karass32.xfolio.data.FiatCurrency
+import pl.karass32.xfolio.data.HistDataResponse
 
 class CoinDetailsViewModel : BaseViewModel() {
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     var coinDataMediator: MediatorLiveData<CoinData>? = null
+    var histData: MutableLiveData<HistDataResponse>? = null
     var currency: MutableLiveData<FiatCurrency> = MutableLiveData()
 
     init {
         currency.value = appDb.fiatCurrencyDao().getCurrency(preferences.getDefaultCurrency()) // TODO MainThread
     }
 
-    fun getCoinData(id: String) : LiveData<CoinData>? {
+    fun getCoinData(coinSymbol: String): LiveData<CoinData>? {
         if (coinDataMediator == null) {
             coinDataMediator = MediatorLiveData()
             coinDataMediator?.addSource(Transformations.switchMap(currency) { _ ->
-                return@switchMap appDb.coinDataDao().getById(id)
+                return@switchMap appDb.coinDataDao().getBySymbol(coinSymbol)
             }) { coinData ->
                 if (preferences.getDefaultCurrency() != "USD") {
                     coinData?.let {
@@ -34,5 +40,26 @@ class CoinDetailsViewModel : BaseViewModel() {
             }
         }
         return coinDataMediator
+    }
+
+    fun getHistData(coinSymbol: String): LiveData<HistDataResponse>? {
+        if (histData == null) {
+            histData = MutableLiveData()
+            loadHistData(coinSymbol)
+        }
+        return histData
+    }
+
+    fun loadHistData(coinSymbol: String) {
+        compositeDisposable.add(cryptoCompareService.getAllHistoricalData(coinSymbol)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            histData?.value = result
+                        },
+                        { error ->
+                        }
+                ))
     }
 }
