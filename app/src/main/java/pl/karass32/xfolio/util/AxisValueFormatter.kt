cf9deb24aa84815656b78ae8a4e3a8b84ec809cf
karas <karass32@gmail.com>
2018-07-18 12:14:02 +0200

    format depends of time period

--- app/src/main/java/pl/karass32/xfolio/util/AxisValueFormatter.kt
+++ app/src/main/java/pl/karass32/xfolio/util/AxisValueFormatter.kt
@@ -3,12 +3,21 @@ package pl.karass32.xfolio.util
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
 
-class AxisValueFormatter : IAxisValueFormatter {
+class AxisValueFormatter(private val timePeriod: Long) : IAxisValueFormatter {
 
-    private val simpleDateFormat = SimpleDateFormat("MMM / YY")
+    private val monthYearFormat = SimpleDateFormat("MMM / YY")
+    private val dayMonthFormat = SimpleDateFormat("dd MMM")
+    private val hourDayFormat = SimpleDateFormat("EEE HH:mm")
 
     override fun getFormattedValue(value: Float, axis: AxisBase?): String {
-        return simpleDateFormat.format(value*1000)
+        val days = TimeUnit.SECONDS.toDays(timePeriod)
+
+        return when {
+            days < 3 -> hourDayFormat.format(value*1000)
+            days < 100 -> dayMonthFormat.format(value*1000)
+            else -> monthYearFormat.format(value*1000)
+        }
     }
 }
