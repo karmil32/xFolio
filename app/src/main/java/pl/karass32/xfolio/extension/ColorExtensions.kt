67fa520890ccc2c1ec333ce79f8c35ac6802103e
karas <karass32@gmail.com>
2018-07-26 14:02:35 +0200

    change AppCompatActivity to BaseFragment

--- app/src/main/java/pl/karass32/xfolio/extension/ColorExtensions.kt
+++ app/src/main/java/pl/karass32/xfolio/extension/ColorExtensions.kt
@@ -1,12 +1,12 @@
 package pl.karass32.xfolio.extension
 
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import pl.karass32.xfolio.base.BaseFragment
 
 /**
  * Created by karas on 18.01.2018.
  */
 
 fun View.getColor(colorRes: Int) = ContextCompat.getColor(this.context, colorRes)
-fun AppCompatActivity.getCompatColor(colorRes: Int) = ContextCompat.getColor(this, colorRes)
+fun BaseFragment.getCompatColor(colorRes: Int) = ContextCompat.getColor(appContext, colorRes)
