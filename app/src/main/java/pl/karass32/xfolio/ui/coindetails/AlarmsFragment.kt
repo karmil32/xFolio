39367cec4891874890f398692084ccfa77859d0e
karas <karass32@gmail.com>
2018-08-07 17:03:26 +0200

    add edittext error

--- app/src/main/java/pl/karass32/xfolio/ui/coindetails/AlarmsFragment.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/coindetails/AlarmsFragment.kt
@@ -109,6 +109,9 @@ class AlarmsFragment : BaseFragment() {
                     val valueChange = mCurrentPrice.valueDiff(BigDecimal(p0.toString()))
                     mView.priceAbovePrcValChange.text = appContext.getString(R.string.coin_alarm_change_prc_val_template, percentageChange, CurrencyUtils.getFormattedPrice(valueChange, preferences.getDefaultCurrency()))
                 }
+                if (BigDecimal(mView.priceAboveEditText.text.toString()) < mCurrentPrice) {
+                    mView.priceAboveEditText.error = appContext.getString(R.string.coin_alarm_price_above_error)
+                }
             }
         })
     }
