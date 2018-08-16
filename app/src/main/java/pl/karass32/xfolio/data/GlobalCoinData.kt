5d73b4f76e82fadd569377798067b920ffe4aa39
karas <karass32@gmail.com>
2018-08-16 15:58:49 +0200

    add getter

--- app/src/main/java/pl/karass32/xfolio/data/GlobalCoinData.kt
+++ app/src/main/java/pl/karass32/xfolio/data/GlobalCoinData.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 /**
  * Created by karas on 24.01.2018.
@@ -13,8 +14,12 @@ import java.math.BigDecimal
 data class GlobalCoinData(@PrimaryKey (autoGenerate = false) val id: Long = 0,
                           @ColumnInfo(name = "total_market_cap_usd") @SerializedName("total_market_cap_usd") var totalMarketCap: Long,
                           @ColumnInfo(name = "total_24h_volume_usd") @SerializedName("total_24h_volume_usd") var total24hVolume: Long,
-                          @ColumnInfo(name = "bitcoin_percentage_of_market_cap") @SerializedName("bitcoin_percentage_of_market_cap") val bitcoinDominance: BigDecimal,
+                          @ColumnInfo(name = "bitcoin_percentage_of_market_cap") @SerializedName("bitcoin_percentage_of_market_cap") private val _bitcoinDominance: BigDecimal,
                           @ColumnInfo(name = "active_currencies") @SerializedName("active_currencies") val activeCurrencies: Int,
                           @ColumnInfo(name = "active_assets") @SerializedName("active_assets") val activeAssets: Int,
                           @ColumnInfo(name = "active_markets") @SerializedName("active_markets") val activeMarkets: Int,
-                          @ColumnInfo(name = "last_updated") @SerializedName("last_updated") val lastUpdated: Long)
+                          @ColumnInfo(name = "last_updated") @SerializedName("last_updated") val lastUpdated: Long) {
+
+    var bitcoinDominance: BigDecimal = _bitcoinDominance
+        get() = _bitcoinDominance.setScale(2, RoundingMode.HALF_UP)
+}
