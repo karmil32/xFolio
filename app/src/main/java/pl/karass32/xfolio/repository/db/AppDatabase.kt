69204f711d538aba3e9945804654e0f08dc6e60e
karas <karass32@gmail.com>
2018-08-17 15:43:40 +0200

    add alarms

--- app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
+++ app/src/main/java/pl/karass32/xfolio/repository/db/AppDatabase.kt
@@ -1,6 +1,7 @@
 package pl.karass32.xfolio.repository.db
 
 import android.arch.persistence.room.*
+import pl.karass32.xfolio.data.CoinAlarm
 import pl.karass32.xfolio.data.CoinData
 import pl.karass32.xfolio.data.FiatCurrency
 import pl.karass32.xfolio.data.GlobalCoinData
@@ -8,7 +9,7 @@ import pl.karass32.xfolio.data.GlobalCoinData
 /**
  * Created by karas on 20.02.2018.
  */
-@Database(entities = [CoinData::class, GlobalCoinData::class, FiatCurrency::class, FavoriteEntity::class], version = 3)
+@Database(entities = [CoinData::class, GlobalCoinData::class, FiatCurrency::class, FavoriteEntity::class, CoinAlarm::class], version = 4)
 @TypeConverters(DbTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
@@ -16,4 +17,5 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun globalCoinDataDao(): GlobalCoinDataDao
     abstract fun fiatCurrencyDao(): FiatCurrencyDao
     abstract fun favNameDao() : FavoritesDao
+    abstract fun alarmsDao() : AlarmsDao
 }
