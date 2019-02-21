74fe9a80d3755e7bc515b981214b79486ab2b900
Kamil <karass32@gmail.com>
2019-02-21 19:40:39 +0100

    remove TabLayout and ViewPager

--- app/src/main/java/pl/karass32/xfolio/ui/coindetails/CoinDetailsActivity.kt
+++ app/src/main/java/pl/karass32/xfolio/ui/coindetails/CoinDetailsActivity.kt
@@ -1,10 +1,8 @@
 package pl.karass32.xfolio.ui.coindetails
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import kotlinx.android.synthetic.main.coin_details_activity.*
 import pl.karass32.xfolio.R
-import pl.karass32.xfolio.adapter.CoinDetailsPagerAdapter
 import pl.karass32.xfolio.base.BaseActivity
 
 class CoinDetailsActivity : BaseActivity() {
@@ -21,16 +19,6 @@ class CoinDetailsActivity : BaseActivity() {
         args?.let {
             mCoinSymbol = args.getString("COIN_SYMBOL")
         }
-
-        val viewPagerAdapter = CoinDetailsPagerAdapter(supportFragmentManager, detailsTabs.tabCount)
-        detailsViewPager.adapter = viewPagerAdapter
-        detailsViewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(detailsTabs))
-        detailsTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                detailsViewPager.currentItem = tab.position
-            }
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-        })
+        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, DetailsFragment()).commit()
     }
 }
